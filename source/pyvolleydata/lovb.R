4fe2c85db9b917c4d1e3b75fde47f5f4bb4889c8
Chowtown <blux2703@gmail.com>
2025-08-07 15:10:37 -0400

    Documentation

--- source/pyvolleydata/lovb.R
+++ source/pyvolleydata/lovb.R
@@ -96,11 +96,45 @@ load_lovb_points_log <- function(seasons = NULL) {
 }
 
 
-#' Title
+#' Load cleaned lovb play-by-play data from the volleydata repository.
 #'
-#' @param seasons
+#' @param seasons int, list of int, or None, optional
+#'               Season(s) to load. By default, None loads all available seasons.
+#'               - int: Single season year (e.g., 2025)
+#'               - list of int: Multiple seasons (e.g., c(2024, 2024), 2024:2025)
+#'               - None: Load all available seasons
+#'
+#'               All years must be 2024 or later.
 #'
 #' @returns
+#' --------------------------------  -----------
+#' Column Name                        Type
+#' --------------------------------  -----------
+#' match_id                           dbl
+#' season                             dbl
+#' match_datetime                     chr
+#' set                                dbl
+#' set_start_time                     chr
+#' set_end_time                       chr
+#' set_duration                       dbl
+#' set_home_score                     dbl
+#' set_away_score                     dbl
+#' event_type                         chr
+#' event_time                         chr
+#' libero_enters                      lgl
+#' team_involved                      chr
+#' libero_jersey_number               dbl
+#' libero_subsitute_jersey_number     dbl
+#' rally_start_time                   chr
+#' rally_end_time                     chr
+#' point_team                         chr
+#' call_approved                      lgl
+#' player_in_jersey_number            dbl
+#' player_out_jersey_number           dbl
+#' challenge_reason                   chr
+#' current_home_score                 dbl
+#' current_away_score                 dbl
+#' --------------------------------  -----------
 #' @export
 #'
 #' @examples
@@ -128,6 +162,55 @@ load_lovb_pbp <- function(seasons = NULL) {
   return(pbp)
 }
 
+#' Load cleaned lovb player boxscore data from the volleydata repository.
+#'
+#' @param seasons int, list of int, or None, optional
+#'               Season(s) to load. By default, None loads all available seasons.
+#'               - int: Single season year (e.g., 2025)
+#'               - list of int: Multiple seasons (e.g., c(2024, 2024), 2024:2025)
+#'               - None: Load all available seasons
+#'
+#'               All years must be 2024 or later.
+#'
+#' @returns
+#' --------------------------------  -----------
+#' Column Name                        Type
+#' --------------------------------  -----------
+#' match_id                           dbl
+#' season                             dbl
+#' match_datetime                     chr
+#' player_id                          dbl
+#' player_name                        chr
+#' first_name                         chr
+#' last_name                          chr
+#' jersey_number                      dbl
+#' primary_position                   dbl
+#' roster_status                      chr
+#' is_foreign                         lgl
+#' is_confederation                   lgl
+#' is_captain                         lgl
+#' is_libero                          lgl
+#' set_1_is_starter                   lgl
+#' set_1_starting_position            dbl
+#' set_2_is_starter                   lgl
+#' set_2_starting_position            dbl
+#' set_3_is_starter                   lgl
+#' set_3_starting_position            dbl
+#' set_4_is_starter                   lgl
+#' set_4_starting_position            dbl
+#' set_5_is_starter                   lgl
+#' set_5_starting_position            dbl
+#' team_name                          chr
+#' team_short_name                    chr
+#' team_code                          chr
+#' team_color                         chr
+#' --------------------------------  -----------
+#' @export
+#'
+#' @examples
+#' `load_lovb_player_boxscore(2024)`
+#' `load_lovb_player_boxscore(2024:2025)`
+#' `load_lovb_player_boxscore()`
 load_lovb_player_boxscore <- function(seasons = NULL) {
   current_year <- as.integer(format(Sys.Date(), "%Y"))
 
@@ -144,8 +227,37 @@ load_lovb_player_boxscore <- function(seasons = NULL) {
   return(player_boxscore)
 }
 
+#' Load cleaned lovb team staff data from the volleydata repository.
+#'
+#' @param seasons int, list of int, or None, optional
+#'               Season(s) to load. By default, None loads all available seasons.
+#'               - int: Single season year (e.g., 2025)
+#'               - list of int: Multiple seasons (e.g., c(2024, 2024), 2024:2025)
+#'               - None: Load all available seasons
+#'
+#'               All years must be 2024 or later.
+#'
+#' @returns
+#' --------------------------------  -----------
+#' Column Name                        Type
+#' --------------------------------  -----------
+#' match_id                           dbl
+#' season                             dbl
+#' match_datetime                     chr
+#' team_name                          chr
+#' staff_type                         chr
+#' full_name                          chr
+#' first_name                         chr
+#' last_name                          chr
+#' --------------------------------  -----------
+#' @export
+#'
+#' @examples
+#' `load_lovb_team_staff(2024)`
+#' `load_lovb_team_staff(2024:2025)`
+#' `load_lovb_team_staff()`
 load_lovb_team_staff <- function(seasons = NULL) {
-  current_year <- as.integer(format(Sys.Date(), "%Y"))
+  current_year <- as.dbleger(format(Sys.Date(), "%Y"))
 
   if (is.null(seasons)) {
     seasons <- 2024:current_year
